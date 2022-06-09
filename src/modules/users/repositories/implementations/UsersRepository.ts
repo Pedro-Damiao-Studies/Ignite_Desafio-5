3b822cedf88cab7165f65bc2061f57ded436ba52
Pedro Henrique Ferreira <pedrohenriquefcd@gmail.com>
2022-06-09 15:03:57 -0300

    Implements methods

--- src/modules/users/repositories/implementations/UsersRepository.ts
+++ src/modules/users/repositories/implementations/UsersRepository.ts
@@ -14,17 +14,22 @@ export class UsersRepository implements IUsersRepository {
   async findUserWithGamesById({
     user_id,
   }: IFindUserWithGamesDTO): Promise<User> {
-    // Complete usando ORM
+    const user = await this.repository.findOne(
+      { id: user_id },
+      { relations: ["games"] }
+      );
+
+      return user ?? new User();
   }
 
   async findAllUsersOrderedByFirstName(): Promise<User[]> {
-    return this.repository.query(); // Complete usando raw query
+    return this.repository.query(`SELECT * FROM users ORDER BY first_name`); // Complete usando raw query
   }
 
   async findUserByFullName({
     first_name,
     last_name,
   }: IFindUserByFullNameDTO): Promise<User[] | undefined> {
-    return this.repository.query(); // Complete usando raw query
+    return this.repository.query(`SELECT u.* FROM users u WHERE LOWER(u.first_name) = LOWER($1) AND LOWER(u.last_name) = LOWER($2)`, [first_name, last_name]); // Complete usando raw query
   }
 }
